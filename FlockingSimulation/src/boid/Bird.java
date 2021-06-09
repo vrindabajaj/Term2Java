f72a400637628a1dc12cbdbef14e630a377cc322
manojbajaj <manoj.bajaj@gmail.com>
2021-06-09 15:00:24 +0100

    boid shape changed

--- FlockingSimulation/src/boid/Bird.java
+++ FlockingSimulation/src/boid/Bird.java
@@ -15,15 +15,15 @@ public class Bird extends AbstractBoid {
 	@Override
 	public void display() {
 		putPenDown();
-		move(25);
+		move(20);
 		putPenUp();
 		turn(180);
-		move(10);
+		move(7);
 		turn(180);
 		turn(150);
 		putPenDown();
 		for (int i = 0; i < 3; i++) {
-			move(20);
+			move(15);
 			turn(120);
 		}
 		putPenUp();
